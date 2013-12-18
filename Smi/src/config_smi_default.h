86dff2c772a79362d2728e9b8683d6b3c31a93f5
Ted Ralphs <ted@lehigh.edu>
2013-12-18 00:55:33 +0000

    temporarily committing release candidate

--- Smi/src/config_smi_default.h
+++ Smi/src/config_smi_default.h
@@ -4,13 +4,13 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define SMI_VERSION "trunk"
+#define SMI_VERSION "0.93.4"
 
 /* Major Version number of project */
-#define SMI_VERSION_MAJOR   9999
+#define SMI_VERSION_MAJOR 0
 
 /* Minor Version number of project */
-#define SMI_VERSION_MINOR   9999
+#define SMI_VERSION_MINOR 93
 
 /* Release Version number of project */
-#define SMI_VERSION_RELEASE 9999
+#define SMI_VERSION_RELEASE 4
