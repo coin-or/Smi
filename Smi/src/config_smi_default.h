3541d0be2fa1102da8af5b90504ccfa912709211
Alan King <kingaj@us.ibm.com>
2014-03-10 16:36:45 +0000

    temporarily committing stable candidate

--- Smi/src/config_smi_default.h
+++ Smi/src/config_smi_default.h
@@ -4,13 +4,13 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define SMI_VERSION "trunk"
+#define SMI_VERSION "0.94"
 
 /* Major Version number of project */
-#define SMI_VERSION_MAJOR   9999
+#define SMI_VERSION_MAJOR 0
 
 /* Minor Version number of project */
-#define SMI_VERSION_MINOR   9999
+#define SMI_VERSION_MINOR 94
 
 /* Release Version number of project */
 #define SMI_VERSION_RELEASE 9999
