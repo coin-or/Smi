2f3244827ec3db21ca63cb732adf66ba68b2ad4f
Alan King <kingaj@us.ibm.com>
2015-10-22 19:36:12 +0000

    restoring Smi/stable/0.96

--- Smi/src/config_smi_default.h
+++ Smi/src/config_smi_default.h
@@ -4,7 +4,7 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define SMI_VERSION "0.96.0"
+#define SMI_VERSION "0.96"
 
 /* Major Version number of project */
 #define SMI_VERSION_MAJOR 0
@@ -13,4 +13,4 @@
 #define SMI_VERSION_MINOR 96
 
 /* Release Version number of project */
-#define SMI_VERSION_RELEASE 0
+#define SMI_VERSION_RELEASE 9999
